f460c2906d32129871da90e8f061c4ba226892db
Thyb0tt0 <danielthybo.rasmussen@gmail.com>
2024-08-18 02:43:09 +0200

    Implement a feature that counts words of frankenstein.txt

--- main.py
+++ main.py
@@ -2,4 +2,12 @@ def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
     print(file_contents)
+    print(f"Number of words: {count_words(file_contents)}")
+
+def count_words(file_contents):
+    word_count = 0
+    words = file_contents.split()
+    for word in words:
+        word_count +=1
+    return word_count
 main()
