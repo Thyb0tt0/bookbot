2b7ccfb7bea4d0b7859ea0db7b1051212c59d034
Thyb0tt0 <danielthybo.rasmussen@gmail.com>
2024-08-18 04:35:16 +0200

    Implement feature that counts each character of frankenstein.txt

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ def main():
         file_contents = f.read()
     print(file_contents)
     print(f"Number of words: {count_words(file_contents)}")
+    print(each_character_count(file_contents))
 
 def count_words(file_contents):
     word_count = 0
@@ -10,4 +11,15 @@ def count_words(file_contents):
     for word in words:
         word_count +=1
     return word_count
+
+def each_character_count(file_contents):
+    count_dict = {}
+    lowered_text = file_contents.lower()
+    for character in lowered_text:
+        if character.isalpha():
+            if character in count_dict:
+                count_dict[character] +=1
+            else:
+                count_dict[character] = 1
+    return count_dict
 main()
