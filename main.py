698db96123c522b73e1f5a0081910524b3f27db9
Thyb0tt0 <danielthybo.rasmussen@gmail.com>
2024-08-18 19:00:30 +0200

    added feature that sorts each character by highest count and now prints a report

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-    print(file_contents)
-    print(f"Number of words: {count_words(file_contents)}")
-    print(each_character_count(file_contents))
+    
+    count_result = each_character_count(file_contents)
+    sorted_result = dict_to_sorted_list(count_result)
+
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{count_words(file_contents)} words found in the document")
+    print("")
+    for key in sorted_result:
+        print(
+            f"The '{key['char']}' character was found {key['count']} times")
+    print("--- End report ---")
 
 def count_words(file_contents):
-    word_count = 0
     words = file_contents.split()
-    for word in words:
-        word_count +=1
+    word_count = len(words)
     return word_count
 
 def each_character_count(file_contents):
@@ -22,4 +28,17 @@ def each_character_count(file_contents):
             else:
                 count_dict[character] = 1
     return count_dict
+
+def dict_to_sorted_list(count_dict):
+    count_list = []
+    for char in count_dict:
+        keys_dict = {}
+        count = count_dict[char]
+        keys_dict['char'] = char
+        keys_dict['count'] = count
+        count_list.append(keys_dict)
+    def sort_on(keys_dict):
+        return keys_dict['count']
+    count_list.sort(reverse=True, key=sort_on)
+    return count_list
 main()
